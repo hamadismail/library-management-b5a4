4b9f5c6ac7175df2e0bd303ef2c22765f408938a
Hamad Ismail <hamad.ismail.gub@gmail.com>
2025-07-03 19:53:05 +0600

    make navbar mobile responsive

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,33 +1,76 @@
 import { Link, NavLink } from "react-router";
 import { ModeToggle } from "./ModeToggle";
 import "./navbar.css";
+import { useState } from "react";
+import { Menu, X } from "lucide-react";
 
 const Navbar = () => {
-  const links = (
-    <>
-      <NavLink to="/" className="hover:text-primary transition-colors">
-        All Books
-      </NavLink>
-      <NavLink to="/create-book" className="hover:text-primary transition-colors">
-        Add Book
-      </NavLink>
-      <NavLink
-        to="/borrow-summary"
-        className="hover:text-primary transition-colors"
-      >
-        Borrow Summary
-      </NavLink>
-    </>
-  );
+  const [open, setOpen] = useState(false);
+
+  const links = [
+    {
+      path: "/",
+      name: "All Books",
+    },
+    {
+      path: "/create-book",
+      name: "Add Book",
+    },
+    {
+      path: "/borrow-summary",
+      name: "Borrow Summary",
+    },
+  ];
+
   return (
     <div className="border-b shadow-sm">
       <nav className="max-w-6xl mx-auto px-4 py-4 flex justify-between items-center">
-        <Link to="/" className="text-2xl font-bold text-primary">
-          LMS
-        </Link>
+        <div className="flex items-center gap-2">
+          {open ? (
+            <X
+              onClick={() => setOpen((prev) => !prev)}
+              className="cursor-pointer md:hidden z-20"
+            />
+          ) : (
+            <Menu
+              onClick={() => setOpen((prev) => !prev)}
+              className="cursor-pointer md:hidden z-20"
+            />
+          )}
+
+          <Link to="/" className="text-2xl font-bold text-primary">
+            LMS
+          </Link>
+        </div>
+
+        {/* For Mobile Menu */}
+        <div
+          className={`${
+            open ? "top-0" : "-top-96"
+          } fixed left-0 w-full bg-muted p-4 z-10 rounded flex flex-col gap-2 items-center shadow transition-all duration-300`}
+        >
+          {links.map((link, idx) => (
+            <NavLink
+              onClick={() => setOpen(false)}
+              className="hover:text-primary transition-colors"
+              key={idx}
+              to={link.path}
+            >
+              {link.name}
+            </NavLink>
+          ))}
+        </div>
 
         <div className="hidden md:flex space-x-8 text-sm font-medium">
-          {links}
+          {links.map((link, idx) => (
+            <NavLink
+              className="hover:text-primary transition-colors"
+              key={idx}
+              to={link.path}
+            >
+              {link.name}
+            </NavLink>
+          ))}
         </div>
 
         <div>
